9fc45d7373b54ff67422e287f28a1be3db14122a
menxipeng <Mxp949544>
2021-02-04 15:03:41 +0800

    feat: 引入 log4j2

--- src/main/java/com/mxp/order/controller/OrderController.java
+++ src/main/java/com/mxp/order/controller/OrderController.java
@@ -1,7 +1,9 @@
 package com.mxp.order.controller;
 
 import com.mxp.cloudapicommon.entity.CommonResult;
+import com.mxp.cloudapicommon.entity.Payment;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -27,6 +29,13 @@ public class OrderController {
     public CommonResult getOrder(HttpServletRequest request){
         //restTemplate.postForObject(URL + "/payment/getPort", request, CommonResult.class);
         return restTemplate.getForObject(URL + "/payment/getPort", CommonResult.class);
+    }
+
+    @RequestMapping("/payment/{id}")
+    public CommonResult getPayment(@PathVariable("id") Integer id){
+        Payment payment = new Payment();
+        payment.setId(id);
+        return restTemplate.postForObject(URL + "/payment/info", payment, CommonResult.class);
     }
 
 }
